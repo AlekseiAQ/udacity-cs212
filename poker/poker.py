877d2b9ad38212f35cf068675a71679db8bf3ae3
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-06-27 22:58:40 -0400

    Updated hand_rank function and tests

--- poker/poker.py
+++ poker/poker.py
@@ -29,63 +29,26 @@ def allmax(iterable, key=None):
 
 
 def hand_rank(hand):
-    """Return a value indicating the ranking of a hand."""
-    ranks = card_ranks(hand)
-    if straight(ranks) and flush(hand):            # straight flush
-        return (8, max(ranks))
-    elif kind(4, ranks):                           # 4 of a kind
-        return (7, kind(4, ranks), kind(1, ranks))
-    elif kind(3, ranks) and kind(2, ranks):        # full house
-        return (6, kind(3, ranks), kind(2, ranks))
-    elif flush(hand):                              # flush
-        return (5, ranks)
-    elif straight(ranks):                          # straight
-        return (4, max(ranks))
-    elif kind(3, ranks):                           # 3 of a kind
-        return (3, kind(3, ranks), ranks)
-    elif two_pair(ranks):                          # 2 pair
-        return (2, two_pair(ranks), ranks)
-    elif kind(2, ranks):                           # kind
-        return (1, kind(2, ranks), ranks)
-    else:                                          # high card
-        return (0, hand)
+    """Return a velue indicating how high the hand ranks."""
+    groups = group(['--23456789TJQKA'.index(rank) for rank, _ in hand])
+    counts, ranks = zip(*groups)
+    if ranks == (14, 5, 4, 3, 2):
+        ranks = (5, 4, 3, 2, 1)
+    straight = len(ranks) == 5 and max(ranks) - min(ranks) == 4
+    flush = len(set([suit for _, suit in hand])) == 1
+    return (9 if (5,) == counts else  # 5 of a kind
+            8 if straight and flush else  # straight flush
+            7 if (4, 1) == counts else  # 4 of a kind
+            6 if (3, 2) == counts else  # full house
+            5 if flush else  # flush
+            4 if straight else  # straight
+            3 if (3, 1, 1) == counts else  # 3 of a kind
+            2 if (2, 2, 1) == counts else  # 2 pair
+            1 if (2, 1, 1, 1) == counts else  # kind
+            0), ranks  # high card
 
 
-def card_ranks(cards):
-    """Return a list of the ranks, sorted with higher first."""
-    ranks = ['--23456789TJQKA'.index(rank) for rank, suit in cards]
-    ranks.sort(reverse=True)
-    return [5, 4, 3, 2, 1] if (ranks == [14, 5, 4, 3, 2]) else ranks
-
-
-def straight(ranks):
-    """Return True if the ordered ranks form a 5-card straight."""
-    # return sorted(range(min(ranks), max(ranks) + 1), reverse=True) == ranks
-    return (max(ranks) - min(ranks) == 4) and len(set(ranks)) == 5
-
-
-def flush(hand):
-    """Return True if all the cards have the same suit."""
-    suits = [suit for _, suit in hand]
-    return len(set(suits)) == 1
-    return kind(r, ranks)
-
-
-def kind(n, ranks):
-    """Return the first rank that this hand has exactly n of.
-    Return None if there is no n-of-a-kind in the hand."""
-    for rank in ranks:
-        if ranks.count(rank) == n:
-            return rank
-    return None
-
-
-def two_pair(ranks):
-    """If there are two pair, return the two ranks as a
-    tuple: (highest, lowest); otherwise return None."""
-    pair = kind(2, ranks)
-    lowpair = kind(2, list(reversed(ranks)))
-    if pair and lowpair != pair:
-        return (pair, lowpair)
-    else:
-        return None
+def group(items):
+    """Return a list of [(count, x)...], highest count first, then highest x first."""
+    groups = [(items.count(x), x) for x in set(items)]
+    return sorted(groups, reverse=True)
--- poker/test_poker.py
+++ poker/test_poker.py
@@ -1,6 +1,6 @@
 import pytest
 
-from poker import card_ranks, flush, hand_rank, kind, poker, straight, two_pair
+from poker import hand_rank, poker
 
 straight_flush = "6c 7c 8c 9c Tc".split()
 four_kind = "9d 9h 9s 9c 7d".split()
@@ -8,30 +8,13 @@ full_house = "Td Tc Th 7c 7d".split()
 two_pair_1 = "5s 5d 9h 9c 6s".split()
 straight_acelow = "AC 2D 4H 3D 5S".split()
 
-four_kind_ranks = card_ranks(four_kind)
-two_pair_ranks = card_ranks(two_pair_1)
-
-CARD_RANKS = (
-    ('arg', 'expected'),
-    [
-        (straight_flush, [10, 9, 8, 7, 6]),
-        (four_kind, [9, 9, 9, 9, 7]),
-        (full_house, [10, 10, 10, 7, 7]),
-    ]
-)
-
-
-@pytest.mark.parametrize(*CARD_RANKS)
-def test_card_ranks(arg, expected):
-    assert card_ranks(arg) == expected
-
 
 HAND_RANK = (
     ('arg', 'expected'),
     [
-        (straight_flush, (8, 10)),
-        (four_kind, (7, 9, 7)),
-        (full_house, (6, 10, 7)),
+        (straight_flush, (8, (10, 9, 8, 7, 6))),
+        (four_kind, (7, (9, 7))),
+        (full_house, (6, (10, 7))),
     ]
 )
 
@@ -39,72 +22,6 @@ HAND_RANK = (
 @pytest.mark.parametrize(*HAND_RANK)
 def test_hand_rank(arg, expected):
     assert hand_rank(arg) == expected
-
-
-KIND = (
-    ('args', 'expected'),
-    [
-        ((4, four_kind_ranks), 9),
-        ((3, four_kind_ranks), None),
-        ((2, four_kind_ranks), None),
-        ((1, four_kind_ranks), 7),
-    ]
-)
-
-
-@pytest.mark.parametrize(*KIND)
-def test_kind(args, expected):
-    assert kind(*args) == expected
-
-
-TWO_PAIR = (
-    ('arg', 'expected'),
-    [
-        (four_kind_ranks, None),
-        (two_pair_ranks, (9, 5)),
-    ]
-)
-
-
-@pytest.mark.parametrize(*TWO_PAIR)
-def test_two_pair(arg, expected):
-    assert two_pair(arg) == expected
-
-
-# assert two_pair(four_kind_ranks) == None
-# assert two_pair(two_pair_ranks) == (9, 5)
-
-STRAIGHT = (
-    ('arg', 'expected'),
-    [
-        (card_ranks(straight_acelow), True),
-        ([9, 8, 7, 6, 5], True),
-        ([9, 8, 8, 6, 5], False),
-    ]
-)
-
-
-@pytest.mark.parametrize(*STRAIGHT)
-def test_straight(arg, expected):
-    assert straight(arg) == expected
-
-
-# assert straight(card_ranks(straight_acelow)) == True
-# assert straight([9, 8, 7, 6, 5]) == True
-# assert straight([9, 8, 8, 6, 5]) == False
-
-FLUSH = (
-    ('arg', 'expected'),
-    [
-        (straight_flush, True),
-        (four_kind, False),
-    ]
-)
-
-
-@pytest.mark.parametrize(*FLUSH)
-def test_flush(arg, expected):
-    assert flush(arg) == expected
 
 
 POKER = (
